cba9edc4849d36acbf4fe90ba3bcc75d7f44be57
Diana Müller <diana.muller@globalhitss.com.br>
2024-06-07 14:40:16 -0300

    Resolved merge conflicts

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+<<<<<<< HEAD
+=======
+import reportWebVitals from './reportWebVitals';
+>>>>>>> 388cb63 (Initialize project using Create React App)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,3 +16,10 @@ root.render(
   </React.StrictMode>
 );
 
+<<<<<<< HEAD
+=======
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+>>>>>>> 388cb63 (Initialize project using Create React App)
